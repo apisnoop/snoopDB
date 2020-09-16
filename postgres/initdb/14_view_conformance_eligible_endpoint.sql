b83baec710476964ca7cba1981c7fc0e093f6d7b
zach mandeville <zz@ii.coop>
2020-09-16 18:41:21 +1200

    update jsons

--- postgres/initdb/14_view_conformance_eligible_endpoint.sql
+++ postgres/initdb/14_view_conformance_eligible_endpoint.sql
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-create or replace view conformance.eligible_endpoint as
-     select endpoint
-       from open_api
-              join (
-                select release
-                  from open_api
-                 order by release::semver desc
-                 limit 1) latest using(release)
-      where level = 'stable'
-     except
-     select endpoint
-       from open_api
-      where path ~~ any('{"%volume%", "%storage%"}')
-         or deprecated is true
-         or k8s_kind = 'ComponentStatus'
-         or (k8s_kind = 'Node' and k8s_action = any('{"delete", "post"}'));
-=======
      create or replace view conformance.eligible_endpoint as
           select endpoint
             from open_api
@@ -33,7 +15,6 @@ create or replace view conformance.eligible_endpoint as
               or k8s_kind = 'ComponentStatus'
               or (k8s_kind = 'Node' and k8s_action = any('{"delete", "post"}'))
               or endpoint = any('{"getFlowcontrolApiserverAPIGroup", "createCoreV1NamespacedServiceAccountToken"}');
->>>>>>> 0e75a3a06710f7b9814633a344771ef4cc7df45f
 
 comment on view conformance.eligible_endpoint is 'all current stable endpoints for which conformant tests could be written, following conformance guidelines';
 
